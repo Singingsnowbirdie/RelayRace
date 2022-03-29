205af5593fb677615f99ae921d82bf0cf96bbeec
Elena Slavina <55747006+Singingsnowbirdie@users.noreply.github.com>
2022-03-29 12:07:26 +0300

    Project loaded

---
+++ Assets/Scripts/Task_01.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//сымитировано движение ловушки с шипами в каком-нибудь данже
+
+public class Task_01 : MonoBehaviour
+{
+    //массив точек
+    Vector3[] points = new Vector3[4]
+    {
+        new Vector3(-4,0,4),
+        new Vector3(4,0,4),
+        new Vector3(4,0,-4),
+        new Vector3(-4,0,-4)
+    };
+
+    [SerializeField] int movingSpeed; //скорость движения
+    [SerializeField] int rotatingSpeed; //скорость вращения
+    Vector3 target; //текущая цель
+    int index = 1; //индекс текущей цели в коллекции
+    bool isMoving = true; //переключатель "движется/не движется"
+    bool isForward = true; //переключатель "вперед/назад"
+
+    void Start()
+    {
+        StartCoroutine(Move());
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.Rotate(0, rotatingSpeed, 0, Space.Self);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) //если нажат пробел
+        {
+            isMoving = !isMoving; //включаем и выключаем движение
+
+            if (!isMoving)
+            {
+                StopAllCoroutines();
+            }
+            else
+            {
+                StartCoroutine(Move());
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E)) //если нажата кнопка "Е"
+        {
+            isForward = !isForward; //переключаем направление движения
+            StopAllCoroutines();
+            SelectTarget();
+            StartCoroutine(Move());
+        }
+    }
+
+    IEnumerator Move()
+    {
+        target = points[index]; //устанавливаем цель
+
+        //пока не приблизились к цели, движемся к ней
+        while (Vector3.Distance(transform.position, target) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, movingSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        //потом выбираем следующую цель
+        SelectTarget();
+
+        //движемся к следующей цели
+        StartCoroutine(Move());
+    }
+
+    void SelectTarget()
+    {
+        if (isForward)
+        {
+            if (index != 3) index++;
+            else index = 0;
+        }
+        else
+        {
+            if (index != 0) index--;
+            else index = 3;
+        }
+    }
+}
---
+++ Assets/Scripts/Task_02.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task_02 : MonoBehaviour
+{
+    [SerializeField] List<Transform> snowmen; //все снеговики
+    [SerializeField] List<Transform> points; //все точки
+    [SerializeField] int speed; //скорость
+
+    int point = 0;
+    int snowmanID = 0;
+
+    Transform currentSnowman; //снеговик, который сейчас бежит
+
+    private void Start()
+    {
+        StartCoroutine(MoveTo()); //запускаем движение
+    }
+
+    IEnumerator MoveTo()
+    {
+        Transform target = points[point]; //выбираем цель
+        currentSnowman = snowmen[snowmanID]; //выбираем снеговика
+        GameObject baton = currentSnowman.transform.GetChild(1).gameObject;
+        baton.SetActive(true);
+        currentSnowman.LookAt(target); //поворачиваем снеговика к цели
+
+        //пока не приблизились к цели, движемся к ней
+        while (Vector3.Distance(currentSnowman.position, target.position) > 0.1f)
+        {
+            currentSnowman.position = Vector3.MoveTowards(currentSnowman.position, target.position, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        point++; //меняем точку
+
+        if (point % 2 == 0) //на нечетной точке меняем снеговика
+        {
+            snowmanID++;
+            baton.SetActive(false);
+        }
+
+        Debug.Log($"point {point}");
+
+        if (point < points.Count) //если это была не последняя точка
+        {
+            StartCoroutine(MoveTo()); //запускаем движение
+        }
+    }
+}
